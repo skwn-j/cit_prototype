04f802f5533d9b6b4e2f24bc713d43a6fddec65e
jeho.oh <jeho1335@gmail.com>
2018-08-26 09:23:01 +0900

    [cit_prototype] merge conflict

--- app/src/main/java/com/example/minim/cit_prototype/Module/Fragment/Tutorial/TutorialFragment.java
+++ app/src/main/java/com/example/minim/cit_prototype/Module/Fragment/Tutorial/TutorialFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.minim.cit_prototype.R;
 
@@ -15,11 +16,12 @@ import org.greenrobot.eventbus.EventBus;
 
 import Common.CommonEventBusObject;
 import Common.ConstVariables;
+import Utils.PreferencesManager;
 
 
-public class TutorialFragment extends Fragment implements View.OnClickListener{
+public class TutorialFragment extends Fragment implements View.OnClickListener {
     private final String TAG = TutorialFragment.class.getSimpleName();
-    private Button mButton1, mButton2, mButton3;
+    private ImageView mButton1, mButton2, mButton3;
 
     public TutorialFragment() {
     }
@@ -44,32 +46,33 @@ public class TutorialFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.button1:
                 mButton1.setSelected(true);
-                if(mButton2.isSelected()){
+                if (mButton2.isSelected()) {
                     mButton2.setSelected(false);
                 }
                 break;
 
             case R.id.button2:
                 mButton2.setSelected(true);
-                if(mButton1.isSelected()){
+                if (mButton1.isSelected()) {
                     mButton1.setSelected(false);
                 }
                 break;
 
             case R.id.cancel_action:
-                if(mButton1.isSelected() && mButton2.isSelected()){
+                if (mButton1.isSelected() && mButton2.isSelected()) {
                     Log.d(TAG, "##### onClick cancel_action ##### multiSelected Exception");
                 }
-                if(mButton1.isSelected()){
+                if (mButton1.isSelected()) {
                     Log.d(TAG, "##### onClick cancel_action ##### mButton1 is selected");
+                    savePref(ConstVariables.Companion.getPREF_AGENT_TYPE_GRAND_CHILD());
 
-                }else if(mButton2.isSelected()) {
+                } else if (mButton2.isSelected()) {
                     Log.d(TAG, "##### onClick cancel_action ##### mButton2 is selected");
+                    savePref(ConstVariables.Companion.getPREF_AGENT_TYPE_FRIEND());
                 }
-                savePref(ConstVariables.Companion.getEVENTBUS_TYPE_TUTORIAL_DONE());
                 break;
         }
 
@@ -77,23 +80,23 @@ public class TutorialFragment extends Fragment implements View.OnClickListener{
 
     private void initializeUI(View v) {
         Log.d(TAG, "##### initializeUI #####");
-        mButton1 = (Button) (Button) v.findViewById(R.id.button1);
+        mButton1 =  v.findViewById(R.id.button1);
         mButton1.setOnClickListener(this);
 
-        mButton2 = (Button) (Button) v.findViewById(R.id.button2);
+        mButton2 =  v.findViewById(R.id.button2);
         mButton2.setOnClickListener(this);
 
-        mButton3 = (Button) (Button) v.findViewById(R.id.cancel_action);
+        mButton3 =  v.findViewById(R.id.cancel_action);
         mButton3.setOnClickListener(this);
 
     }
 
-    private void savePref(int type){
+    private void savePref(int type) {
         Log.d(TAG, "##### savePRef ##### type : " + type);
-        if(type == ConstVariables.Companion.getPREF_AGENT_TYPE_FRIEND()){
-            //PreferencesManager.INSTANCE.saveIntegerPreferencesco(getActivity(), ConstVariables.Companion.getPREF_KEY_AGENT_TYPE(), ConstVariables.Companion.getPREF_AGENT_TYPE_GRAND_CHILD());
-        }else if(type == ConstVariables.Companion.getPREF_AGENT_TYPE_GRAND_CHILD()){
-            //PreferencesManager.INSTANCE.saveIntegerPreferencesco(getActivity(), ConstVariables.Companion.getPREF_KEY_AGENT_TYPE(), ConstVariables.Companion.getPREF_AGENT_TYPE_FRIEND());
+        if (type == ConstVariables.Companion.getPREF_AGENT_TYPE_FRIEND()) {
+            PreferencesManager.INSTANCE.saveIntegerPreferencesco(getActivity(), ConstVariables.Companion.getPREF_KEY_AGENT_TYPE(), ConstVariables.Companion.getPREF_AGENT_TYPE_GRAND_CHILD());
+        } else if (type == ConstVariables.Companion.getPREF_AGENT_TYPE_GRAND_CHILD()) {
+            PreferencesManager.INSTANCE.saveIntegerPreferencesco(getActivity(), ConstVariables.Companion.getPREF_KEY_AGENT_TYPE(), ConstVariables.Companion.getPREF_AGENT_TYPE_FRIEND());
         }
         EventBus.getDefault().post(new CommonEventBusObject(ConstVariables.Companion.getEVENTBUS_TYPE_TUTORIAL_DONE()));
     }
