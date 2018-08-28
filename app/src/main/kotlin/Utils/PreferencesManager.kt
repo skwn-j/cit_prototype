42d449818c65189f3f597197bdc7758d6ec1b0db
jeho.oh <jeho1335@gmail.com>
2018-08-28 10:31:36 +0900

    [cit_prototype] update source

--- app/src/main/kotlin/Utils/PreferencesManager.kt
+++ app/src/main/kotlin/Utils/PreferencesManager.kt
@@ -9,7 +9,7 @@ import android.util.Log
 object PreferencesManager {
     val TAG = this.javaClass.simpleName
 
-    fun saveIntegerPreferencesco(context : Context, key : String, value : Int){
+    fun saveIntegerPreferencesco(context: Context, key: String, value: Int) {
         Log.d(TAG, "##### savePreferences #####")
         val pref = PreferenceManager.getDefaultSharedPreferences(context)
         val editor = pref.edit()
@@ -17,10 +17,18 @@ object PreferencesManager {
         editor.apply()
     }
 
-    fun loadIntegerSharedPreferences(context : Context, key : String) : Int{
+    fun loadIntegerSharedPreferences(context: Context, key: String): Int {
         Log.d(TAG, "##### laodIntegerSharedPreferences #####")
 
         val pref = PreferenceManager.getDefaultSharedPreferences(context)
         return pref.getInt(key, ConstVariables.PREF_AGENT_TYPE_NONE)
+    }
+
+    fun clearPreference(context: Context) {
+        Log.d(TAG, "##### clearPreference #####")
+        val pref = PreferenceManager.getDefaultSharedPreferences(context)
+        val editor = pref.edit()
+        editor.clear()
+        editor.apply()
     }
 }
